5ec5dec522a23713edcd6569627b11fae42a482a
jwk7 <johnwooyoungkim@gmail.com>
2024-03-20 19:10:05 -0700

    fixing some stuff

--- dmbrl/utils/DataFunctions.py
+++ dmbrl/utils/DataFunctions.py
@@ -15,8 +15,28 @@ def Translation1DImage(I0: np.ndarray, x: float, G: np.ndarray) -> np.ndarray:
     xs[xs==0] = -1
     xs = xs*x
     Ix = np.matmul(scipy.linalg.expm(xs*G),I0)
-<<<<<<< HEAD
     return xs,Ix
-=======
-    return xs,Ix
->>>>>>> 8087237a3cd80c1dbf3a539460c2d78da0522f14
+
+
+@staticmethod
+# Print iterations progress
+def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    """
+    Call in a loop to create terminal progress bar
+    @params:
+        iteration   - Required  : current iteration (Int)
+        total       - Required  : total iterations (Int)
+        prefix      - Optional  : prefix string (Str)
+        suffix      - Optional  : suffix string (Str)
+        decimals    - Optional  : positive number of decimals in percent complete (Int)
+        length      - Optional  : character length of bar (Int)
+        fill        - Optional  : bar fill character (Str)
+        printEnd    - Optional  : end character (e.g. "\r", "\r\n") (Str)
+    """
+    percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = fill * filledLength + '-' * (length - filledLength)
+    print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+    # Print New Line on Complete
+    if iteration == total: 
+        print()
--- dmbrl/utils/MBExp.py
+++ dmbrl/utils/MBExp.py
@@ -1,11 +1,17 @@
 import dmbrl.utils.Agent as Agent
+import time
+from dmbrl.utils.DataFunctions import printProgressBar
+    
 class MBExperiment:
     def __init__(self,params):
         self.params = params
 
     def run_experiment(self):
+
+        printProgressBar(0, self.params.exp_cfg.iteration, prefix = 'Progress:', suffix = 'Complete', length = 50)
         for i in range(self.params.exp_cfg.iteration):
             agent = Agent.Agent(self.params)
             agent.train()
+            printProgressBar(i + 1, self.params.exp_cfg.iteration, prefix = 'Progress:', suffix = 'Complete', length = 50)
         agent.test()
         pass
